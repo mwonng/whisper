8d6b05be1ed185584d3562cc9036bdab860b396f
Michael Wonng <michael@wonng.com>
2018-05-30 22:14:35 +1000

    add pre tag for source markdown

--- pages/post.js
+++ pages/post.js
@@ -8,24 +8,25 @@ import styled from 'styled-components';
 const ContentWrapper = styled.div`
   max-width: 1000px;
   margin: auto;
+  min-height: 60vh;
 `
 const LeftContent = styled.div`
   width: 50%;
   display: inline-block;
-  vertical-align: top;
+  vertical-align: middle;
 `
 
 const RightContent = styled.div`
   width: 50%;
   display: inline-block;
-  vertical-align: top;
+  vertical-align: middle;
 `
 
 const Content = (props) => {
   return(
     <ContentWrapper>
       <LeftContent><Markdown source={atob(props.content)} /></LeftContent>
-      <RightContent>{atob(props.content)}</RightContent>
+      <RightContent><pre>{atob(props.content)}</pre></RightContent>
     </ContentWrapper>
   )
 }
